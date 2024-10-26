34bb3f9b0a1dd86e0fd2521acb086d205b19240d
sambit4590 <sambitp36@gmail.com>
2024-10-26 19:25:07 +0530

    Modify the code of Header  Component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,74 +1,76 @@
 import React, { useState } from "react";
 import { FaBars } from "react-icons/fa6";
 import { IoClose } from "react-icons/io5";
-import { Link } from 'react-router-dom'
-
-
+import { Link } from "react-router-dom";
 
 function Header() {
-  const [toggle, setToggle] = useState(true);
+  const [toggle, setToggle] = useState(false);
 
   return (
     <div className="bg-blue-400 p-4">
       <div className="max-w-[1240px] py-[20px] flex justify-between mx-auto items-center">
+        {/* Logo */}
         <div className="font-bold text-3xl">
-          <Link  index to="/home">LOGO</Link>
+          <Link to="/home">LOGO</Link>
         </div>
 
-        {toggle ? (
-          <IoClose
-            onClick={() => setToggle(!toggle)}
-            className="text-3xl md:hidden block"
-          />
-        ) : (
-          <FaBars
-            onClick={() => setToggle(!toggle)}
-            className="text-3xl md:hidden block"
-          />
-        )}
+        {/* Toggle Button for Small Screens */}
+        <div className="md:hidden z-50">
+          {toggle ? (
+            <IoClose
+              onClick={() => setToggle(!toggle)}
+              className="text-3xl cursor-pointer"
+            />
+          ) : (
+            <FaBars
+              onClick={() => setToggle(!toggle)}
+              className="text-3xl cursor-pointer"
+            />
+          )}
+        </div>
 
-        <ul className="text-xl font-semibold hidden md:flex gap-10">
-        <li>
-                    <Link  to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/resources">Resources</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-
-          {/* <li>Home</li>
-          <li>About</li>
-          <li>Resources</li>
-          <li>Contact</li> */}
+        {/* Navigation Links */}
+        <ul className="hidden md:flex text-xl font-semibold gap-10">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
         </ul>
 
-        {/*Responsive menu*/}
-
+        {/* Responsive Menu Overlay for Small Screens */}
         <ul
-          className={`duration-300 w-full h-screen md:hidden fixed bg-blue-300 text-black top-[105px] 
-        ${toggle ? "left-[0]" : "left-[100%]"}`}
+          className={`fixed top-0 left-0 w-full h-full bg-blue-300 text-black flex flex-col justify-center items-center space-y-8 text-2xl font-semibold transition-transform duration-300 ease-in-out ${
+            toggle ? "translate-x-0" : "translate-x-full"
+          } md:hidden z-40`}
         >
-          <li className="p-5">
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="p-5">
-                    <Link to="/about">About</Link>  
-                </li>
-                <li className="p-5">
-                    <Link to="/resources">Resources</Link>
-                </li>
-                <li className="p-5"> 
-                    <Link to="/contact">Contact</Link>
-                </li>
-          {/* <li className="p-5">Home</li>
-          <li className="p-5">About</li>
-          <li className="p-5">Resources</li>
-          <li className="p-5">Contact</li> */}
+          <li>
+            <Link onClick={() => setToggle(false)} to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link onClick={() => setToggle(false)} to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link onClick={() => setToggle(false)} to="/resources">
+              Resources
+            </Link>
+          </li>
+          <li>
+            <Link onClick={() => setToggle(false)} to="/contact">
+              Contact
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
